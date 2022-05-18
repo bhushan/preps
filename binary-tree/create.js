78edde49bcabfb49bc4826c37845f20a5a4f5638
Bhushan <bhushangaykawad@gmail.com>
2022-05-19 02:13:05 +0530

    [BinaryTree] Inorder traversal and code improvements added

--- binary-tree/create.js
+++ binary-tree/create.js
@@ -8,6 +8,10 @@ const execute = (nodes) => {
   return tree.build(nodes);
 };
 
-const preorderSequencedArray = [2, 2, 5, -1, -1, 3, -1, 6, -1, 1];
+const preorderSequencedArray = [1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1];
 
-console.log({ root: execute(preorderSequencedArray) });
+//   1
+//  2  3
+// 4 5  6
+
+console.log({ root: JSON.stringify(execute(preorderSequencedArray), null, 4) });
--- binary-tree/inorder-traversal.js
+++ binary-tree/inorder-traversal.js
@@ -1,22 +1,23 @@
-// Traverse a Binary tree using Preorder sequence
+// Traverse a Binary tree using Inorder sequence
 
 let BinaryTree = require("./BinaryTree");
 
-// Preorder Traversal means:
-// Start from root node
-// Then traverse to the left
-// Then traverse to the right
-// Repeat same for all nodes
+// Inorder Traversal means:
+// First we print all the left node
+// Then we print root node
+// Then we print right node
 // Print nothings when node is null
-const preorderTraversal = (root) => {
+const inorderTraversal = (root) => {
   if (root === null) {
     return;
   }
 
+
+  inorderTraversal(root.left);
+
   console.log({ data: root.data });
 
-  preorderTraversal(root.left);
-  preorderTraversal(root.right);
+  inorderTraversal(root.right);
 };
 
 const execute = (nodes) => {
@@ -24,7 +25,7 @@ const execute = (nodes) => {
 
   let root = tree.build(nodes);
 
-  preorderTraversal(root);
+  inorderTraversal(root);
 };
 
 const preorderSequencedArray = [1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1];
@@ -33,5 +34,5 @@ const preorderSequencedArray = [1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1];
 //  2  3
 // 4 5  6
 
-// output: 1 2 4 5 3 6
+// output: 4 2 5 1 3 6
 console.log({ root: execute(preorderSequencedArray) });
