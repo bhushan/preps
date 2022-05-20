eccd576529d01135564c74ba73ae1fbf35109e8a
Bhushan <bhushangaykawad@gmail.com>
2022-05-20 22:05:12 +0530

    [BinaryTree] Check if tree is binary search tree or not

--- binary-tree/validation.js
+++ binary-tree/validation.js
@@ -14,15 +14,50 @@
 //      / \   \
 //     1   6    14
 //       /  \    \
-//      4   7     13
+//      4   7     18
 
 let BinaryTree = require("./BinaryTree");
 
-const preorderSequencedArray = [
-  8, 3, 1, -1, -1, 6, 4, -1, -1, 7, -1, -1, 10, -1, 14, -1, 13,
+const validPreorderSequencedArray = [
+  8, 3, 1, -1, -1, 6, 4, -1, -1, 7, -1, -1, 10, -1, 14, -1, 18,
 ];
 
 let tree = new BinaryTree();
-let root = tree.build(preorderSequencedArray);
+let validBSTRootNode = tree.build(validPreorderSequencedArray);
 
-// console.log({tree: JSON.stringify(root, null, 4)});
+const invalidPreorderSequencedArray = [
+  8, 3, 1, -1, -1, 6, 4, -1, -1, 7, -1, -1, 10, -1, 14, -1, 13,
+];
+
+let invalidBST = new BinaryTree();
+let invalidBSTRootNode = invalidBST.build(invalidPreorderSequencedArray);
+
+function isBST(root, min, max) {
+  if (root === null) {
+    return true;
+  }
+
+  // console.log({ root: root.data, min, max, minC: root.data <= min, maxC: root.data >= max });
+
+  if (root.data <= min || root.data >= max) {
+    return false;
+  }
+
+  const isLeftValid = isBST(root.left, min, root.data);
+  const isRightValid = isBST(root.right, root.data, max);
+
+  return isLeftValid && isRightValid;
+}
+
+// console.log({ tree: JSON.stringify(validRootNode, null, 4) });
+console.log({
+  isBST: isBST(validBSTRootNode, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER),
+});
+// console.log({tree: JSON.stringify(invalidRootNode, null, 4)});
+console.log({
+  isBST: isBST(
+    invalidBSTRootNode,
+    Number.MIN_SAFE_INTEGER,
+    Number.MAX_SAFE_INTEGER
+  ),
+});
