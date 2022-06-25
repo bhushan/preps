d8e132ea2e57568c4e6947c525b43785bd03c17a
Sameer <saupadhyayula@paypal.com>
2022-06-25 14:50:33 +0530

    [binary-search]:Kth-sum-value, maximize-the-fraction, color-balls, readme

---
+++ cpp/binary-search/maximize-the-fraction/solution.cpp
@@ -0,0 +1,26 @@
+#include <stack>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+using namespace std;
+#include <math.h>
+#define int long long
+#define float long double
+#define endl '\n'
+
+//input that works across all functions
+
+
+void solve(){
+    cout<<"I am Sameer Kumar"<<endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+//    int _t;cin>> _t; while(_t--)
+    solve();
+}
