15643f6076f0f231a1ba23bc6cf8962e7ad0a861
Bhushan <bhushangaykawad@gmail.com>
2022-10-09 18:56:33 +0530

    [Linked-List]: improved comments for better understanding

--- javascript/linked-list/reverse-linked-list.js
+++ javascript/linked-list/reverse-linked-list.js
@@ -12,19 +12,46 @@ const dataNodes = [1, 2, 3, 4, 5];
 const list = new SinglyLinkedList();
 const head = list.build(dataNodes);
 
-const reverse = (head) => {
-  let newHead = null;
-
-  while (head != null) { // loop until head becomes null ie reaches last node
-    const next = head.next; // temporarily store next node to next variable
-    head.next = newHead; // now heads next becomes new head
-    newHead = head; // new head is updated with current head
-    head = next; // store next node as head only for next iteration
+const reverseIterativeApproach = (head) => {
+  if (head === null || head.next === null) {
+    return head;
   }
 
-  return newHead;
+  let previousHead = null;
+
+  while (head != null) {
+    // loop until head becomes null ie reaches last node
+
+    // head is 1 so next becomes 2
+    const nextHead = head.next;
+
+    // Remove linking of 1 -> 2 and make it 1 -> NULL
+    head.next = previousHead;
+
+    // for the next iteration previousHead becomes 1
+    previousHead = head;
+
+    // head ie 1 becomes 2 for next iteration
+    head = nextHead;
+  }
+
+  return previousHead;
 };
 
 // Given Linked List 1 -> 2 -> 3 -> 4 -> 5 -> NULL
 // Expected Linked List NULL <- 1 <- 2 <- 3 <- 4 <- 5
-console.log({ result: JSON.stringify(reverse(head), null, 4) });
+// ie Linked List 5 -> 4 -> 3 -> 2 -> 1 -> NULL
+console.log({
+  result: JSON.stringify(reverseIterativeApproach(head), null, 4),
+});
+
+console.log({
+  expected: "null",
+  result: JSON.stringify(reverseIterativeApproach(null), null, 4),
+});
+
+const singleNodeList = list.build([1]);
+
+console.log({
+  result: JSON.stringify(reverseIterativeApproach(singleNodeList), null, 4),
+});
